9f8c41617f1a5d139044c9f3b502023611b369b1
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2023-02-02 20:54:55 -0600

    third commit

---
+++ noteApp/src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,57 @@
+package com.devmountain.noteApp.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+    //Create a private type Long  'id' field w/ 'Id' and 'GeneratedValue' Annotation
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //Create a private type String 'body' field w/ 'Column' Annotation
+    @Column(columnDefinition = "text")
+    private String body;
+
+    //Create a private type 'User' field called 'user'  w/ '@ManyToOne' and '@JsonBackReference' Annotation
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    //Generate a  Getter & Setter for the  private (type Long) 'id' field
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    //Generate a  Getter & Setter for the  private (type String) 'body' field
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    //This Constructor is commented out since the '@NoArgsConstructor' Annotation is implemented on line 13
+    //Generate a Constructor w/ No fields (No Arguments)
+            //    public Note() {
+            //
+            //    }
+//Generate a Constructor w/ All fields (ALL Arguments): 'id' and 'body'
+    public Note(Long id, String body) {
+        this.id = id;
+        this.body = body;
+    }
+}
--- noteApp/src/main/java/com/devmountain/noteApp/entities/User.java
+++ noteApp/src/main/java/com/devmountain/noteApp/entities/User.java
@@ -1,9 +1,69 @@
 package com.devmountain.noteApp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name="users")
+@Table(name="Users")
 public class User {
+    //In the curly braces add the ID annotation "@Id"(in order to give the class an id) with a 'GeneratedValue' annotation w/ a strategy option defined as 'GenerationType.IDENTITY'
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;      //Create a  new 'private' member variable (aka 'field') called 'id' and is of type 'Long'
+
+    //Create another private member variable (aka 'field') called 'username' and is of type 'String' and annotate is with the '@Column' annotation w/ a 'unique' option set to 'true'
+    @Column(unique = true)
+    private String username;
+
+    //Create another private member variable (aka 'field') called 'password' and is of type 'String' with no options enabled.
+    @Column
+    private String password;
+
+    //Create a private Set of type Note called 'noteSet' set equal to 'HashSet'
+    //Annotate the Set w 'OneToMany' mappedBy option to 'user', then the fetch option set to 'FetchType.LAZY'....
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JsonManagedReference
+    private Set<Note> noteSet = new HashSet<>();
+
+    //Generate a  Getter & Setters for the above created fields : 'id', 'username', and 'password'
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    //Generate a Constructor with no fields (No Arguments) -'Select None' (on the Generate menu popup)
+    public User() {
+
+    }
+
+    //Generate a Constructor for the following fields (w/Arguments) previously created : 'id, username, password'
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
 }
+
